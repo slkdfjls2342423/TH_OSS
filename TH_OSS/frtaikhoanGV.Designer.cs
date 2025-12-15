ccf468b5ace732ea5d59138f83a49e6a2f42ea36
Dang Manh Tan <dangmanhtan0@gmail.com>
2025-12-15 09:07:26 +0700

    fix giao dien form ql tai khoan giang vien

--- TH_OSS/frtaikhoanGV.Designer.cs
+++ TH_OSS/frtaikhoanGV.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvtkgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnKhoitao = new System.Windows.Forms.Button();
+            this.Lecturers = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtlecturerid = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtkgv)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(411, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Thoát\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvtkgv);
+            this.groupBox1.Location = new System.Drawing.Point(12, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 274);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // dgvtkgv
+            // 
+            this.dgvtkgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvtkgv.Location = new System.Drawing.Point(16, 21);
+            this.dgvtkgv.Name = "dgvtkgv";
+            this.dgvtkgv.RowTemplate.Height = 24;
+            this.dgvtkgv.Size = new System.Drawing.Size(458, 236);
+            this.dgvtkgv.TabIndex = 0;
+            this.dgvtkgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvtkgv_CellContentClick);
+            this.dgvtkgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvtkgv_CellContentClick);
+            this.dgvtkgv.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvtkgv_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(141, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TÀI KHOẢN GIẢNG VIÊN";
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnThem.Location = new System.Drawing.Point(311, 333);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 34);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnXoa.Location = new System.Drawing.Point(311, 373);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 34);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSua.Location = new System.Drawing.Point(311, 413);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 34);
+            this.btnSua.TabIndex = 5;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnKhoitao
+            // 
+            this.btnKhoitao.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnKhoitao.Location = new System.Drawing.Point(311, 453);
+            this.btnKhoitao.Name = "btnKhoitao";
+            this.btnKhoitao.Size = new System.Drawing.Size(75, 34);
+            this.btnKhoitao.TabIndex = 6;
+            this.btnKhoitao.Text = "Khởi tạo";
+            this.btnKhoitao.UseVisualStyleBackColor = false;
+            this.btnKhoitao.Click += new System.EventHandler(this.btnKhoitao_Click);
+            // 
+            // Lecturers
+            // 
+            this.Lecturers.AutoSize = true;
+            this.Lecturers.Location = new System.Drawing.Point(8, 31);
+            this.Lecturers.Name = "Lecturers";
+            this.Lecturers.Size = new System.Drawing.Size(74, 17);
+            this.Lecturers.TabIndex = 8;
+            this.Lecturers.Text = "LecturerID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Password";
+            // 
+            // txtlecturerid
+            // 
+            this.txtlecturerid.Location = new System.Drawing.Point(103, 31);
+            this.txtlecturerid.Name = "txtlecturerid";
+            this.txtlecturerid.Size = new System.Drawing.Size(136, 22);
+            this.txtlecturerid.TabIndex = 11;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(103, 67);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(136, 22);
+            this.txtusername.TabIndex = 12;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(103, 103);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(136, 22);
+            this.txtpassword.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtpassword);
+            this.groupBox2.Controls.Add(this.Lecturers);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtusername);
+            this.groupBox2.Controls.Add(this.txtlecturerid);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 334);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 153);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            // 
+            // frtaikhoanGV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(513, 496);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnKhoitao);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "frtaikhoanGV";
             this.Text = "frtaikhoanGV";
+            this.Load += new System.EventHandler(this.frtaikhoanGV_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtkgv)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvtkgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label Lecturers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtlecturerid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKhoitao;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
     }
 }
